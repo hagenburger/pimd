bb397320c6284287a6e94e983f8b59eae56cce25
Nico Hagenburger <nico@hagenburger.net>
2018-05-07 21:41:21 +0200

    Render code with info strings

--- test/document.test.js
+++ test/document.test.js
@@ -35,3 +35,62 @@ describe('Meta information', () => {
     expect(doc.title).to.be.null
   })
 })
+
+describe('Code blocks', () => {
+  const testBothCodeBlockTypes = (input, callback) => {
+    callback(input)
+    callback(input.replace(/~~~/g, '```'))
+  }
+
+  it('should render a code block', () => {
+    const input = unindent`
+      ~~~
+      My code
+      ~~~
+    `
+    testBothCodeBlockTypes(input, (input) => {
+      const doc = new Document(input)
+      const html = doc.render()
+      expect(html).to.match(/<pre><code[^>]*>\s*My code/)
+    })
+  })
+
+  it('should render a code block with language', () => {
+    const input = unindent`
+      ~~~ html
+      <p>My code</p>
+      ~~~
+    `
+    testBothCodeBlockTypes(input, (input) => {
+      const doc = new Document(input)
+      const html = doc.render()
+      expect(html).to.match(/<pre><code[^>]+class="[^"]*lang-html[^"]*"[^>]*>\s*&lt;p&gt;My code/)
+    })
+  })
+
+  it('should render a code block with language as file extension', () => {
+    const input = unindent`
+      ~~~ my-File_name.html
+      <p>My code</p>
+      ~~~
+    `
+    testBothCodeBlockTypes(input, (input) => {
+      const doc = new Document(input)
+      const html = doc.render()
+      expect(html).to.match(/<pre><code[^>]+class="[^"]*lang-html[^"]*"[^>]*>\s*&lt;p&gt;My code/)
+    })
+  })
+
+  it('should render a code block with language and additional info string without issues', () => {
+    const input = unindent`
+      ~~~ html info string
+      <p>My code</p>
+      ~~~
+    `
+    testBothCodeBlockTypes(input, (input) => {
+      const doc = new Document(input)
+      const html = doc.render()
+      expect(html).to.match(/<pre><code[^>]+class="[^"]*lang-html[^"]*"[^>]*>\s*&lt;p&gt;My code/)
+    })
+  })
+})
