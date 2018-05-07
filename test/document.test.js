37f01a44caca7dcbe12a752a3c3448795140dc75
Nico Hagenburger <nico@hagenburger.net>
2018-05-07 20:34:48 +0200

    Parse document title out of first headline

--- test/document.test.js
+++ test/document.test.js
@@ -13,3 +13,25 @@ describe('Rendering documents', () => {
     expect(html).to.match(new RegExp(output))
   })
 })
+
+describe('Meta information', () => {
+  it('should use the first headline as the title', () => {
+    const input = unindent`
+      # Hello
+
+      # World
+    `
+    const doc = new Document(input)
+    expect(doc.title).to.equal('Hello')
+  })
+
+  it('should not have a title when no headline is used', () => {
+    const input = unindent`
+      Hello
+
+      World
+    `
+    const doc = new Document(input)
+    expect(doc.title).to.be.null
+  })
+})
