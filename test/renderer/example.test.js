5f1ffa6986bfdda4dd3b848ed52467d94e4f62dc
Nico Hagenburger <nico@hagenburger.net>
2018-05-12 19:38:08 +0200

    Move code renderer in separate file

---
+++ test/renderer/example.test.js
@@ -0,0 +1,44 @@
+const Example = require('../../lib/renderer/example')
+
+describe('Rendering of example blocks', () => {
+  it('should render', () => {
+    const code = new Example({}, 'My code', 'text')
+    const html = code.render()
+    expect(html)
+      .to.have.selector('code')
+      .with.text.to.equal('My code')
+  })
+
+  it('should escape HTML', () => {
+    const code = new Example({}, '<p>My code</p>', 'text')
+    const html = code.render()
+    expect(html)
+      .to.have.selector('code')
+      .with.text.to.equal('<p>My code</p>')
+    expect(html)
+      .to.have.selector('code')
+      .with.innerHTML.to.equal('&lt;p&gt;My code&lt;/p&gt;')
+  })
+
+  it('should render with class names', () => {
+    const code = new Example({ langPrefix: 'lang-' }, 'My code', 'mycustomlang')
+    const html = code.render()
+    expect(html)
+      .to.have.selector('.pimd-example .pimd-code pre > code.lang-mycustomlang')
+      .with.text.to.equal('My code')
+  })
+
+  it('should use a custom lang prefix', () => {
+    const code = new Example({ langPrefix: 'langlang-' }, 'My code', 'text')
+    const html = code.render()
+    expect(html)
+      .to.have.selector('.pimd-example .pimd-code pre > code.langlang-text')
+  })
+
+  it('should ignore lang prefix', () => {
+    const code = new Example({ langPrefix: undefined }, 'My code', 'text')
+    const html = code.render()
+    expect(html)
+      .to.have.selector('.pimd-example .pimd-code pre > code.text')
+  })
+})
