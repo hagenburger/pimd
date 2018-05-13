00ce98e9edb6089a905bb084d814402d2332d10e
Nico Hagenburger <nico@hagenburger.net>
2018-05-13 13:53:52 +0200

    Add hooks to Fragment

--- test/fragment.test.js
+++ test/fragment.test.js
@@ -18,3 +18,27 @@ describe('Rendering of fragments', () => {
     expect(html).to.have.selector('p.test')
   })
 })
+
+describe('Hooks', () => {
+  it('should execute beforeRender hook', () => {
+    const fragment = new Fragment()
+    fragment.addHook('beforeRender', function () {
+      this.element.textContent = 'Hook'
+    })
+    const html = fragment.render()
+    expect(html)
+      .to.have.selector('div')
+      .with.text.to.equal('Hook')
+  })
+
+  it('should execute afterRender hook', () => {
+    const fragment = new Fragment()
+    fragment.addHook('afterRender', function () {
+      this.outerHTML = '<p>Hook</p>'
+    })
+    const html = fragment.render()
+    expect(html)
+      .to.have.selector('p')
+      .with.text.to.equal('Hook')
+  })
+})
