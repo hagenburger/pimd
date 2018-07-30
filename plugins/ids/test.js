24e95c71d54b861b4698ba3ce52cc63571f2c1a5
V <36138541+artnerdnet@users.noreply.github.com>
2018-07-30 21:27:21 +0200

    Beautified and added semicolons

--- plugins/ids/test.js
+++ plugins/ids/test.js
@@ -1,29 +1,28 @@
-const {Document} = require('../..');
+const {
+  Document
+} = require('../..');
 const plugin = require('.');
-
-
 describe('Info strings', () => {
-  it('should add an id to a headline', () => {
-    const input = unindent`
-      # Test <?: #myid ?>
-    `
-    const doc = new Document(input)
-    doc.config.use(plugin)
-    const html = doc.render()
+  it('should add an ID to a headline', () => {
+      const input = unindent `
+  # Test <?: #my-id ?>
+`;
+      const doc = new Document(input);
+      doc.config.use(plugin);
+      const html = doc.render();
       expect(html)
-      .to.have.selector('h1#myid')
-  })
-
-  it('should add an id to a code block', () => {
-    const input = unindent`
-      ~~~ html #myid
-      <p>Lorem</p>
-      ~~~
-    `
-    const doc = new Document(input)
-    doc.config.use(plugin)
-    const html = doc.render()
-    expect(html)
-      .to.have.selector('div#myid')
-  })
-})
+          .to.have.selector('h1#my-id');
+  });
+  it('should add an ID to a code block', () => {
+      const input = unindent `
+  ~~~ html #my-id
+  <p>Lorem</p>
+  ~~~
+`;
+      const doc = new Document(input);
+      doc.config.use(plugin);
+      const html = doc.render();
+      expect(html)
+          .to.have.selector('div#my-id');
+  });
+});
