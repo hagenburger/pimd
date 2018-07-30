96ed0f1c03968ffd6f433899bc472a7ed997e3dc
V <36138541+artnerdnet@users.noreply.github.com>
2018-07-30 07:02:32 +0200

    ID plugin

---
+++ plugins/ids/index.js
@@ -0,0 +1,6 @@
+module.exports = function (config) {
+  config.addInfoStringParser(/#([^ ]+)/, function (match, id) {
+    this.element.id = id
+  })
+}
+
---
+++ plugins/ids/test.js
@@ -0,0 +1,29 @@
+const {Document} = require('../..');
+const plugin = require('.');
+
+
+describe('Info strings', () => {
+  it('should add an id to a headline', () => {
+    const input = unindent`
+      # Test <?: #myid ?>
+    `
+    const doc = new Document(input)
+    doc.config.use(plugin)
+    const html = doc.render()
+      expect(html)
+      .to.have.selector('h1#myid')
+  })
+
+  it('should add an id to a code block', () => {
+    const input = unindent`
+      ~~~ html #myid
+      <p>Lorem</p>
+      ~~~
+    `
+    const doc = new Document(input)
+    doc.config.use(plugin)
+    const html = doc.render()
+    expect(html)
+      .to.have.selector('div#myid')
+  })
+})
