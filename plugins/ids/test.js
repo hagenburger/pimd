bd614f25352bd2b0d5eac182d5b10cbfdcde62b2
V <36138541+artnerdnet@users.noreply.github.com>
2018-07-30 07:02:32 +0200

    Introduce ID plugin

---
+++ plugins/ids/test.js
@@ -0,0 +1,41 @@
+const {Document} = require('../..')
+const plugin = require('.')
+
+describe('Info strings', () => {
+  it('should add an ID to a headline', () => {
+    const input = unindent`
+      # Test <?: #myid ?>
+    `
+    const doc = new Document(input)
+    doc.config.use(plugin)
+    const html = doc.render()
+    expect(html)
+      .to.have.selector('h1#myid')
+  })
+
+  it('should add an ID to a code block', () => {
+    const input = unindent`
+      ~~~ html #myid
+      <p>Lorem</p>
+      ~~~
+    `
+    const doc = new Document(input)
+    doc.config.use(plugin)
+    const html = doc.render()
+    expect(html)
+      .to.have.selector('div#myid')
+  })
+
+  it('should add an ID to a table', () => {
+    const input = unindent`
+      | Table <?: #th-id ?>      | Table                    |
+      |-- -- -- -- -- -- -- -- --|-- -- -- -- -- -- -- -- --|
+      | Table                    | Table <?: #td-id ?>      |
+    `
+    const doc = new Document(input)
+    doc.config.use(plugin)
+    const html = doc.render()
+    expect(html)
+      .to.have.selector('p#td-id')
+  })
+})
