d59eb7ede53ac73b726c75f20431a2288d19c1dc
artnerdnet <artnerdnet@gmail.com>
2018-08-20 19:30:00 +0200

    Working version of live demo

--- demo/js/script.js
+++ demo/js/script.js
@@ -1,16 +1,10 @@
 var { Document } = require('pimd');
 
-const mdInput = document.getElementById("markdownInput");
-window.onload = function (){
-    mdInput.innerHTML = `# Try out the PIMD project
-    All of the source code is completely free and open, available on GitHub under MIT licence. We are porting from Ruby to Javascript (see our Collaboration section for more info on how to help).
-    `;
-};
-
 const userInput = document.getElementById("myInput");
 userInput.addEventListener('input',  function(e) {
     e.preventDefault();
-    const contentString = document.getElementById("myInput").value;  
+    const myInput = document.getElementById("myInput").value;
+    const contentString = `${myInput}`;
     convertWithPimd(contentString);
 }
 );
