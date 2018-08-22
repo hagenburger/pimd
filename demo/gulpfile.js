b2593610f4c5e9dbca5484fbcc74690c63ab254d
artnerdnet <artnerdnet@gmail.com>
2018-08-22 10:25:27 +0200

    PIMD issue #14 - live demo

---
+++ demo/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+connect = require('gulp-connect');
+ 
+gulp.task('connect', function() {
+  connect.server();
+});
+gulp.task('default', ['connect']);
+
+var bro = require('gulp-bro');
+gulp.task('build', () =>
+ gulp.src('js/script.js')
+   .pipe(bro())
+   .pipe(gulp.dest('js_dist'))
+)
+
+gulp.watch('js/*.js', ['build'])
