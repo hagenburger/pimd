7e168af343bab624f82c0925cdb5ee3dda2d250d
Nico Hagenburger <nico@hagenburger.net>
2018-09-02 17:07:27 +0200

    Implement HTML injector plugin
    Resolves #18.

---
+++ plugins/html-injector/index.js
@@ -0,0 +1,60 @@
+module.exports = function(config) {
+  config.hooks.add("example:init", "html-injector", example => {
+    example.insertions = []
+    example.insertAt = function(offset, html) {
+      example.insertions.push({
+        offset: offset,
+        html: html
+      })
+    }
+  })
+
+  config.hooks.add(
+    "example:afterHighlight",
+    "html-injector",
+    (example, code) => {
+      return processInsertions(code, example.insertions)
+    }
+  )
+}
+
+function processInsertions(code, insertions) {
+  insertions = insertions.sort((a, b) => a.offset > b.offset)
+  let codeWithHighlights = ""
+  let offset = 0
+  let nextPosition = insertions.shift()
+  let insideCharacter = false
+  let insideHtml = false
+
+  for (let i = 0; i <= code.length; i++) {
+    const character = code[i] || ""
+    if (character === "<") {
+      insideHtml = true
+      if (nextPosition && offset == nextPosition.offset) {
+        codeWithHighlights += nextPosition.html
+        nextPosition.html = ""
+      }
+    } else if (insideHtml && character === ">") {
+      insideHtml = false
+    } else if (!insideHtml) {
+      if (nextPosition && offset === nextPosition.offset) {
+        codeWithHighlights += nextPosition.html
+        nextPosition = insertions.shift()
+      }
+      if (character === "&") {
+        insideCharacter = true
+        if (nextPosition && offset == nextPosition.offset) {
+          codeWithHighlights += nextPosition.html
+          nextPosition.html = ""
+        }
+      } else if (insideCharacter && character === ";") {
+        insideCharacter = false
+        offset++
+      } else if (!insideCharacter) {
+        offset++
+      }
+    }
+    codeWithHighlights += character
+  }
+  return codeWithHighlights
+}
