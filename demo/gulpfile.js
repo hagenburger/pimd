926210aa092605327a0281bdd69f928b151507d2
artnerdnet <artnerdnet@gmail.com>
2018-09-07 22:09:48 +0200

    Fixed html syntax

---
+++ demo/gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require("gulp")
+const connect = require("gulp-connect")
+
+gulp.task("connect", function() {
+  connect.server()
+})
+gulp.task("default", ["connect"])
+
+var bro = require("gulp-bro")
+gulp.task("build", () =>
+  gulp
+    .src("js/script.js")
+    .pipe(bro())
+    .pipe(gulp.dest("js_dist"))
+)
+
+gulp.watch("js/*.js", ["build"])
