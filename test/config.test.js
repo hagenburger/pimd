9507e19c417fc96a749fae25ca22630c3de48887
Nico Hagenburger <nico@hagenburger.net>
2018-09-09 03:19:41 +0200

    Introduce info string commands

--- test/config.test.js
+++ test/config.test.js
@@ -29,6 +29,101 @@ describe("Info strings", () => {
   })
 })
 
+describe("Info string commands", () => {
+  it("should add commands to info strings", () => {
+    const infoString = "xx +test4 xx"
+    const config = new Config()
+    config.addInfoStringCommand("test4", function(match) {
+      this.element.id = "id4"
+    })
+    const fragment = new Fragment()
+
+    config.parseInfoString(infoString, fragment)
+    expect(fragment.render()).to.have.selector('div[id="id4"]')
+  })
+
+  it("should add commands to info strings with regexp", () => {
+    const infoString = "xx +test5=/ab\\/c/g xx"
+    const config = new Config()
+    const func = sinon.spy()
+    config.addInfoStringCommand("test5", { types: ["regexp"] }, func)
+    const fragment = new Fragment()
+
+    config.parseInfoString(infoString, fragment)
+    expect(func).to.have.been.calledWith(/ab\/c/g)
+  })
+
+  it("should add commands to info strings with double quote string", () => {
+    const infoString = 'xx +test7="ab\\"c" xx'
+    const config = new Config()
+    const func = sinon.spy()
+    config.addInfoStringCommand("test7", { types: ["string"] }, func)
+    const fragment = new Fragment()
+
+    config.parseInfoString(infoString, fragment)
+    expect(func).to.have.been.calledWith('ab"c')
+  })
+
+  it("should add commands to info strings with number", () => {
+    const infoString = "xx +test9=-1234.5 xx"
+    const config = new Config()
+    const func = sinon.spy()
+    config.addInfoStringCommand("test9", { types: ["number"] }, func)
+    const fragment = new Fragment()
+
+    config.parseInfoString(infoString, fragment)
+    expect(func).to.have.been.calledWith(-1234.5)
+  })
+
+  it("should add commands to info strings with true", () => {
+    const infoString = "xx +test10=true xx"
+    const config = new Config()
+    const func = sinon.spy()
+    config.addInfoStringCommand("test10", { types: ["boolean"] }, func)
+    const fragment = new Fragment()
+
+    config.parseInfoString(infoString, fragment)
+    expect(func).to.have.been.calledWith(true)
+  })
+
+  it("should add commands to info strings with false", () => {
+    const infoString = "xx +test11=false xx"
+    const config = new Config()
+    const func = sinon.spy()
+    config.addInfoStringCommand("test11", { types: ["boolean"] }, func)
+    const fragment = new Fragment()
+
+    config.parseInfoString(infoString, fragment)
+    expect(func).to.have.been.calledWith(false)
+  })
+
+  it("should add commands to info strings with null", () => {
+    const infoString = "xx +test12=null xx"
+    const config = new Config()
+    const func = sinon.spy()
+    config.addInfoStringCommand("test12", { types: ["null"] }, func)
+    const fragment = new Fragment()
+
+    config.parseInfoString(infoString, fragment)
+    expect(func).to.have.been.calledWith(null)
+  })
+
+  it("should add commands to info strings with multiple values", () => {
+    const infoString = "xx +test13=23,/a,b,c/gi,true xx"
+    const config = new Config()
+    const func = sinon.spy()
+    config.addInfoStringCommand(
+      "test13",
+      { types: ["boolean", "number", "regexp"], multiple: true },
+      func
+    )
+    const fragment = new Fragment()
+
+    config.parseInfoString(infoString, fragment)
+    expect(func).to.have.been.calledWith(23, /a,b,c/gi, true)
+  })
+})
+
 describe("Pluings", () => {
   it("should load plugins", () => {
     const testFunction = function(pi) {
