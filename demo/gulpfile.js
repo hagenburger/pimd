00ecce329357503176d37d8b1cf7bc9523a5982f
V <36138541+artnerdnet@users.noreply.github.com>
2018-09-23 18:34:00 +0200

    Update gulpfile.js
    Deleted duplicated code

--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -2,28 +2,6 @@ const gulp = require("gulp")
 const connect = require("gulp-connect")
 const bro = require("gulp-bro")
 const runSequence = require("run-sequence")
-
-gulp.task("default", function(callback) {
-  runSequence("build", "connect", callback)
-})
-
-gulp.task("connect", function() {
-  connect.server()
-})
-
-gulp.task("build", () =>
-  gulp
-    .src("js/script.js")
-    .pipe(bro())
-    .pipe(gulp.dest("js_dist"))
-)
-
-gulp.watch("js/*.js", ["build"])
-
-const gulp = require("gulp")
-const connect = require("gulp-connect")
-const bro = require("gulp-bro")
-const runSequence = require("run-sequence")
 const deploy = require('gh-pages');
 
 gulp.task("default", function(callback) {
