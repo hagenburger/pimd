22e9ff380ce6acc7a6d9eb0e58314917a3fb2542
artnerdnet <artnerdnet@gmail.com>
2018-09-23 11:42:16 +0200

    deploy live demo to github pages

--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -19,3 +19,35 @@ gulp.task("build", () =>
 )
 
 gulp.watch("js/*.js", ["build"])
+
+const gulp = require("gulp")
+const connect = require("gulp-connect")
+const bro = require("gulp-bro")
+const runSequence = require("run-sequence")
+const deploy = require('gh-pages');
+
+gulp.task("default", function(callback) {
+  runSequence("build", "connect", callback)
+})
+
+gulp.task("connect", function() {
+  connect.server()
+})
+
+gulp.task("build", () =>
+  gulp
+    .src("js/script.js")
+    .pipe(bro())
+    .pipe(gulp.dest("js_dist"))
+)
+
+gulp.watch("js/*.js", ["build"])
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp
+    .src("./dist/**/*")
+    .pipe(deploy())
+});
