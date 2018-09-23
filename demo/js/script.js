bb5b78b2d745ba1052b768dfdd58d5b98556d9f0
V <36138541+artnerdnet@users.noreply.github.com>
2018-09-23 11:12:13 +0200

    Update script.js
    There was an unnecesary variable getting an element for the second time

--- demo/js/script.js
+++ demo/js/script.js
@@ -3,8 +3,7 @@ var { Document } = require("pimd")
 const userInput = document.getElementById("my-input")
 userInput.addEventListener("input", function(e) {
   e.preventDefault()
-  const myInput = document.getElementById("my-input").value
-  const contentString = `${myInput}`
+  const contentString = userInput.value
   convertWithPimd(contentString)
 })
 
