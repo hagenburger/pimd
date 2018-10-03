1c57c38f90045dcdddacec6ebd824a8f1fd25623
Diana Vilé <diana.vile@gmail.com>
2018-10-03 15:16:11 +0200

    fix: change name to empty string as default value

--- plugins/attribute/index.js
+++ plugins/attribute/index.js
@@ -1,6 +1,6 @@
 module.exports = function (config) {
   config.addInfoStringParser(/\[([a-z0-9-]+)(?:=([^]+))?\]/i, function (match, name, value) {
-    value = value || name
+    value = value || ""
     if (value.match(/^(".*"|'.*')$/)) value = value.substring(1, value.length - 1)
     this.element.setAttribute(name, value)
   })
